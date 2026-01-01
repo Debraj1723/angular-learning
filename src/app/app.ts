cfe3f805ba8bd98ecb58cae45833f2d3236e2173
Debraj1723 <debraj.basak23@gmail.com>
2026-01-01 22:37:53 +0530

    input and signals learnt

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { User } from "./user/user";
+import { User } from './user/user';
+import { DUMMY_USERS } from '../app/dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,6 @@ import { User } from "./user/user";
 })
 export class App {
   protected readonly title = signal('angular-project-1');
+
+  users = DUMMY_USERS;
 }
